5ab0320304bac868eb31aa5ff10636e0e8d8316e
Sarah Greene <sarah2000greene@gmail.com>
2022-10-25 11:16:16 -0400

    Fetch API results and display in console

--- src/Components/SearchAndFetch.jsx
+++ src/Components/SearchAndFetch.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import { type } from "@testing-library/user-event/dist/type";
 export function SearchAndFetch () {
+    const [pokemons, setPokemons] = useState([]);
+
+    const getPokemon = async() => {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+        const data = await response.json();
+        console.log(data.results);
+        setPokemons(data.results);
+    }
 
     return (
         <>
             <h1>Pokemon Finder</h1>
             <input></input>
-            <button>Search</button>
+            <button onClick={getPokemon}>Search</button>
         </>
     );
 }
