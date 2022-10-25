c7e156b7be0e955374dd22b3134d1181907bf697
Sarah Greene <sarah2000greene@gmail.com>
2022-10-25 09:24:19 -0400

    first commit

---
+++ src/Components/SearchAndFetch.jsx
@@ -0,0 +1,10 @@
+export function SearchAndFetch () {
+
+    return (
+        <>
+            <h1>Pokemon Finder</h1>
+            <input></input>
+            <button>Search</button>
+        </>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SearchAndFetch } from './Components/SearchAndFetch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    < SearchAndFetch/>
   </React.StrictMode>
 );
 
