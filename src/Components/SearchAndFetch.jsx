748fa3a25497a5cd5946d35e4c83e243ce3fe674
Sarah Greene <sarah2000greene@gmail.com>
2022-10-27 16:15:57 -0400

    Update API to get specific pokemon

--- src/Components/SearchAndFetch.jsx
+++ src/Components/SearchAndFetch.jsx
@@ -2,28 +2,23 @@ import { useState } from "react";
 export function SearchAndFetch () {
     const [pokemons, setPokemons] = useState([]);
     const [input, setInput] = useState("");
-    const [filteredPokemon, setFiltered] = useState([]);
+    //const [filteredPokemon, setFiltered] = useState([]);
 
     //get full set of pokemon data
     const getPokemon = async() => {
         //get data from API
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+        //const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`);
         const data = await response.json();
         console.log(data.results);
         setPokemons(data.results);
 
-        //extract pokemon based on input
-        if (input.length == 0) {
-            console.log('No results found');
-        } else {
-            pokemons.filter(pokemon=>pokemon.include(input)).map(filteredPokemon => filteredPokemon.name);
-        }
     }
 
     //get input field and update state of that input
     const parseData = (e) => {
         setInput(e.target.value);
-        console.log(input);
+        console.log(e.target.value);
     }
 
     return (
@@ -31,7 +26,7 @@ export function SearchAndFetch () {
             <h1>Pokemon Finder</h1>
             <input type="text" placeholder="Search Pokemon" onChange={parseData} value={input}/>
             <button onClick={getPokemon}>Search</button>
-            <div>{filteredPokemon}</div>
+            {/* <div>{filteredPokemon}</div> */}
         </>
     );
 }
